2391c1106265667d1b3b218571f51fce5749a31e
rohl0079 <Luke.A.Rohl@gmail.com>
2017-10-02 23:09:12 -0500

    Reo has movement

--- REO.js
+++ REO.js
@@ -1,86 +1,153 @@
 var reo;
 var game_pieces = [];
 var score;
+var timer;
+
+document.addEventListener('keydown', function(event) {
+  switch(event.keyCode){
+    case 65://left
+      reo.x_speed = -5;
+      break;
+    case 68://right
+      reo.x_speed = 5;
+      break;
+    case 32://jump
+      reo.y_speed = -5;
+      break;
+    }
+});
+document.addEventListener('keyup',function(event) {
+  switch(event.keyCode)
+  {
+    case 65://left
+      reo.x_speed = 0;
+      break;
+    case 68://right
+      reo.x_speed = 0;
+      break;
+    case 32://jump
+      reo.y_speed = 0;
+      break;
+    case 27://Escape
+      alert("Escape");
+      break;
+  }
+});
 
 function start_game() {
-  reo = new component(30, 30, "red", 5, 570, "player");
-  reo.gravity = 0;
+  reo = new component(50, 50, "red", 100, 100, "player");
+
   _2d_scroller.start();
+  //reo.movement.update_pos();
+  reo.movement.update();
+  timer = setInterval(function()
+          {
+            _2d_scroller.clear()
+            reo.movement.update_pos();
+            reo.movement.update();
+          }, 20);
 }
 
 var _2d_scroller = {
-  canvas: document.createElement("canvas"),
+  canvas: document.getElementById("game_canvas"),
   start: function() {
-    this.canvas.width = 600;
-    this.canvas.height = 400;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    var canvas = document.getElementById("game_canvas");
+    canvas.width = 600;
+    canvas.height = 400;
+    this.context = canvas.getContext("2d");
+    document.body.insertBefore(canvas, document.body.childNodes[0]);
     this.num_frames = 0;
-    this.interval = setInterval(updateGame(), 16);
+    this.interval = setInterval(update_game(), 16);
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 };
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  this.score = 0;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
-  this.gravity = 0;
-  this.gravity_speed = 0;
-  if (this.type == "player") {
-    this.input = {
-      left: false,
-      right: false,
-      jump: false
-    };
-  }
-  this.update = function() {
-    ctx = GamePad.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  };
-  this.update_pos = function() {
-    this.gravity_speed += this.gravity;
-    this.x += this.x_speed;
-    this.y += this.y_speed + this.gravity_speed;
-    this.check_stops();
-  };
-  this.check_stops = function() {
-    var bottom = GamePad.canvas.height - this.height;
-    if (this.y > bottom) {
-      this.y = bottom;
-      this.gravity_speed = 0;
-    }
+class component {
+  constructor(width, height, color, x, y, type)
+  {
+    this.score = 0;
 
-    if (this.x <= 5) {
-      this.x = 5;
-      this.x_speed = 0; //Potentially a problem, might get stuck when touching wall.
+    this.x = x;
+    this.y = y;
+
+    this.color = color;
+    this.type = type;
+
+
+    this.width = width;
+    this.height = height;
+
+    this.x_speed = 0;
+    this.y_speed = 0;
+
+    this.gravity = 1;
+    this.gravity_speed = 0;
+
+    this.movement = {
+      update : function()
+      {
+
+        var ctx = _2d_scroller.canvas.getContext("2d");
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+      },
+      update_pos : function()
+      {
+          //reo.gravity_speed += reo.gravity;
+          x += reo.x_speed;
+          reo.y_speed += reo.gravity;
+          y += reo.y_speed;
+          this.check_stops();
+      },
+      check_stops : function() {
+          var bottom = _2d_scroller.canvas.height - height;
+          if (y > bottom) {
+            y = bottom;
+            this.gravity_speed = 0;
+          }
+
+          if (x <= 1) {
+            x = 1;
+            this.x_speed = 0; //Potentially a problem, might get stuck when touching wall.
+          } else if (x >= _2d_scroller.canvas.width - reo.width)
+          {
+            x = _2d_scroller.canvas.width - reo.width;
+            this.x_speed = 0;
+          }
+        },
+
+        check_crash : function(check) {
+          var left = x;
+          var right = x + width;
+          var top = y;
+          var bottom = y + height;
+          var check_left = check.x;
+          var check_right = check.x + check.width;
+          var check_top = check.y;
+          var check_bottom = check.y + check.height;
+          var crash = true;
+          if ((bottom < check_top) || (top > check_bottom)
+              || (right < check_left) || (left > check_right)) {
+            crash = false;
+          }
+          return crash;
+        }
+    };
+
+    if (this.type == "player") {
+      this.input = {
+        left: false,
+        right: false,
+        jump: false
+      };
     }
-  };
-  this.check_crash = function(check) {
-    var left = this.x;
-    var right = this.x + this.width;
-    var top = this.y;
-    var bottom = this.y + this.height;
-    var check_left = check.x;
-    var check_right = check.x + check.width;
-    var check_top = check.y;
-    var check_bottom = check.y + check.height;
-    var crash = true;
-    if ((bottom < check_top) || (top > check_bottom) || (right < check_left) || (left > check_right)) {
-      crash = false;
-    }
-    return crash;
-  };
+  }
 }
 
 function update_game() {
-  var x;
+  var x = 0;
 
 }
 
